1707b9fe0d6288a58ad935078e2d07d93d8de690
xuhongwei <skhizein0706@gmail.com>
2014-04-19 20:47:29 +0800

    修改了session初始化和配置的位置

--- site_helper.py
+++ site_helper.py
@@ -9,8 +9,6 @@ sys.setdefaultencoding("utf-8")
 import web
 import config
 web.config.debug = config.getConfig().debug
-web.config.session_parameters['timeout'] = config.getConfig().timeout
-print web.config.session_parameters['timeout']
 #全局网站映射, 每一个url的partner, webpy会自动加上^$,所以对于剩余uri，强制使用(.*)铺获
 #每一个类，在webpy的application中都是递归的导入相应的类，如:
 #a.b.c.d.E  E是文件中的类，则:
@@ -31,7 +29,9 @@ render = web.template.render
 main_render = render(loc='templates/main', base='base', globals={})
 
 #创建session对象，并进行初始化
-session = web.session.Session(app, web.session.DiskStore('sessions/test'), initializer = {'isLogin': 0 , 'isSuper':'NOT'})
+session = web.session.Session(app, web.session.DiskStore('sessions/'), initializer = {'is_login':False , 'is_super':False})
+#设置session过期时间
+web.config.session_parameters['timeout'] = config.getConfig().session_timeout
 #创建一个被web.loadhook加载的处理器(processor)
 def session_hook():
     web.ctx.session = session
