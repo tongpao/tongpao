862f6dc2fcd3a186a754d5967633c141d98dfc0f
xuhongwei <skhizein0706@gmail.com>
2014-04-19 20:46:35 +0800

    修改了了变量名，is_login

--- controllers/main/login.py
+++ controllers/main/login.py
@@ -20,7 +20,7 @@ class Login():
             @return 返回渲染的模板页面
             否则直接跳转到主页
         """
-        if web.ctx.session.isLogin != 1:
+        if not web.ctx.session.is_login:
             result = {'status':'success'}
 
             return sher.main_render.login(result)
@@ -29,19 +29,19 @@ class Login():
 
     def POST(self):
         #得到用户输入表单中的username,password
-        inputDate = web.input()
-        username = inputDate['username']
-        password= inputDate['password']
+        request_data = web.input()
+        username = request_data['username']
+        password = request_data['password']
 
         #得到model中user对象
         user = User()
         #从数据库中查出所有用户的帐号和密码，返回一个字典类型users
-        users = user.getUsers({'username':username, 'passwd':password},'u_id')
+        row = user.getUserBy({'username':username, 'passwd':password}, fields = 'u_id')
 
         #判断用户输入是否存在并正确
         #若正确则为其创建session表示已登录
-        if users:
-            web.ctx.session.isLogin = 1
+        if row:
+            web.ctx.session.is_login = True
             result =  {'status':'success',}
 
             raise web.seeother('/', False)
@@ -51,7 +51,7 @@ class Login():
 
 class Logout():
     def GET(self):
-        web.ctx.session.isLogin = 0
+        web.ctx.session.is_login = False
         web.ctx.session.kill()
 
         raise web.seeother('/', False)
