d8782d84e0c72f3a6cbe3dd9cc46698c56bbe100
xuhongwei <skhizein0706@gmail.com>
2014-04-19 20:05:27 +0800

    修改了login类，添加了session和logout.

--- controllers/main/login.py
+++ controllers/main/login.py
@@ -7,10 +7,8 @@ reload(sys)
 sys.setdefaultencoding("utf8")
 
 import site_helper as sher
-from modules.main.user import User#,News
+from modules.main.user import User
 import web
-#from web import form
-#from utils import utils
 
 """
 用户登录类
@@ -18,32 +16,43 @@ import web
 class Login():
 
     def GET(self):
-        """接受URL请求，返回给用户登录页面
+        """接受URL请求，判断用户是否已登录，如果未登录返回给用户登录页面
             @return 返回渲染的模板页面
+            否则直接跳转到主页
         """
-        return sher.main_render.login()
+        if web.ctx.session.isLogin != 1:
+            result = {'status':'success'}
+
+            return sher.main_render.login(result)
+
+        raise web.seeother('/', False)
 
     def POST(self):
         #得到用户输入表单中的username,password
-        #username = self.login_form['username'].value
-        #password = self.login_form['password'].value
-        a = web.input()
-        username = a['username']
-        password= a['password']
+        inputDate = web.input()
+        username = inputDate['username']
+        password= inputDate['password']
 
-        #将他们放入到字典中
-        validate_dict = {'username':username,'passwd':password}
         #得到model中user对象
         user = User()
         #从数据库中查出所有用户的帐号和密码，返回一个字典类型users
-        users = user.getUsers({'created':'0'},'username,passwd')
+        users = user.getUsers({'username':username, 'passwd':password},'u_id')
+
         #判断用户输入是否存在并正确
-        if validate_dict in users:
+        #若正确则为其创建session表示已登录
+        if users:
+            web.ctx.session.isLogin = 1
+            result =  {'status':'success',}
 
             raise web.seeother('/', False)
 
-        return sher.main_render.login()
+        result =  {'status':'failure',}
+        return sher.main_render.login(result)
 
+class Logout():
+    def GET(self):
+        web.ctx.session.isLogin = 0
+        web.ctx.session.kill()
 
-
+        raise web.seeother('/', False)
 
