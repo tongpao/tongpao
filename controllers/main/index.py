f5180ee32ce154ca1bfbe8193194f3ffdbb9f996
xiaobing <xiaobingzhang29@gmail.com>
2014-04-19 21:52:32 +0800

    add html and css and js

--- controllers/main/index.py
+++ controllers/main/index.py
@@ -7,11 +7,8 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 import site_helper as sher
-from modules.main.news import News 
 class Index():
     def GET(self):
-        new = News()
-        news = new.getNews()
 
-        return sher.main_render.index(news)
+        return sher.main_render.index()
         
