b1465c61a0d65ccad4a52ac866909afd59131f22
wangyu <wo190810401@gmail.com>
2014-04-19 20:21:55 +0800

    注册系统基本完成，还要继续改进

--- controllers/main/register.py
+++ controllers/main/register.py
@@ -16,13 +16,35 @@ class Register():
 
     def POST(self):
         data = web.input()
-        del data['sub']
-        del data['repasswd']
+
         user = User()
-        user.create_user(data)
-        raise web.seeother('/', False)
+        row = user.getUserBy({'username':data['username'], 'email':data['email']})
+        if row is None:
+            print 'not regitser'
+            user.create_user(data)
+            return sher.main_render.login()
+        else:
+            raise web.seeother('register',False)
 
 
-
-        
-
+#
+#        username= data['username']
+#        email=data['email']
+#        user = User()
+#        validata_user={'username':username}
+#        validata_email={'email':email} 
+#        getusername=user.getUsers({'created':'0'},'username')
+#        getemail=user.getUsers({'created':'0'},'email')
+#        if validata_user in getusername:
+#        elif validata_email in getmail:
+#            raise web.seeother('/',False)
+#
+#        else:
+#
+#
+##        raise web.seeother('/', False)
+#
+#
+#
+#        
+#
