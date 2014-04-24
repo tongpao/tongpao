35a05d80f56673c4ad6917a7b6546db2ab2adace
khakux <zhexuejiadiwang@gmail.com>
2014-04-24 17:00:11 +0800

    修改留言板方法

--- controllers/main/message.py
+++ controllers/main/message.py
@@ -7,7 +7,7 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 import site_helper as sher
-from modules.main.message import Message as Msg
+from modules.main.message import M_Message
 import web
 from utils import utils
 class Message():
@@ -16,9 +16,9 @@ class Message():
     def POST(self):
         data = web.input()
         del data['sub']
-        mes = Msg()
+        mes = M_Message()
         mes.create_message(data)
 
-        raise web.seeother('/message',False)
+        raise web.seeother('/message/list',False)
 
 
